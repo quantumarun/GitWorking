593371b9495594bf67e3885d96b612110fa4106e
Arun Gupta <arun@Aruns-MacBook-Pro.local>
2017-01-27 00:11:46 +0530

    Changes to Feature/Profile

--- GitWorkingDemo/GitWorkingDemo/ViewController.swift
+++ GitWorkingDemo/GitWorkingDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("viewDidLoad")
+        print("ViewDidLoadAgain")
     }
 
     override func didReceiveMemoryWarning() {
